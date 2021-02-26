9dd9f49e9f59c17cf2bc53572f49fc13a390f61b
Lulo02 <luloalbarracin@gmail.com>
2021-02-25 21:59:10 -0500

    se hizo el switch grande y el punto 2

--- Codigos_informe_2/src/codigos_informe_2/Calculos.java
+++ Codigos_informe_2/src/codigos_informe_2/Calculos.java
@@ -4,28 +4,59 @@ package codigos_informe_2;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- *
- * @author Lulo
- */
+    
 public class Calculos {
-    protected float varianza;
-    protected float desviacion_e;
-    protected float suma;
-    protected float media;
+    protected double varianza;
+    protected double desviacion_e;
+    protected double media;
     protected int tamaño;
-    protected int lugar;
     protected int [] arreglo;
+    protected String cadena;
     
-
+    
+    
+Scanner entra= new Scanner(System.in);
   
-
+    public void Llenar_letras(){
+        
+        
+        System.out.println("Ingrese su cadena: ");
+        cadena=entra.nextLine();
+        int longitud=cadena.length();
+        char caracteres[]= cadena.toCharArray();
+        System.out.println(caracteres);
+    }
     
-    public void Llenar (int lugar){
+    public void Repeticiones(){
+        
+        int contador=0;
+        char caracter=0;
+        while(cadena.length()!=0){
+            int contadorAux=0;
+            for (int j = 0; j < cadena.length(); j++) { // recorremos la cadena para contar los caracteres del indice cero
+         if (cadena.charAt(0) == cadena.charAt(j)) {
+             contadorAux++;
+         }
+     }
+            if (contadorAux > contador) { // si el contador del nuevo caracter es mayor al que ya estaba guardado, lo cambiamos
+         contador = contadorAux;
+         caracter = cadena.charAt(0);
+     }
+             
+               
+ }
+    System.out.println(caracter + ": " + contador);
+    cadena = cadena.replaceAll(cadena.charAt(0) + "", "");
+             cadena = cadena.replace('a',caracter).replace('e',caracter).replace('i',caracter).replace('o',caracter).replace('u',caracter);
+             System.out.println(cadena);
+        }
+    
+    public void Llenar_numeros (){
         Scanner entra= new Scanner (System.in);
         System.out.println("Ingrese el tamaño del arreglo");
                        
         tamaño=entra.nextInt();
+        int lugar=0;
                 
         arreglo = new int[tamaño];
         for (int i=0; i<tamaño; i++){
@@ -41,7 +72,7 @@ public class Calculos {
     
     
     public  void Media(){
-            
+            double suma=0;
             for(int i=0;i<tamaño;i++){
             
                 suma=suma+arreglo[i];
@@ -49,48 +80,49 @@ public class Calculos {
                  System.out.println("La suma de los numeros es:" +suma);
                  media=suma/tamaño;
                  System.out.println("La media aritmetica es: "+media);
+                 
                                
     }
         
     public void Varianza(){
-       
+       double suma=0;
         for(int i=0;i<tamaño;i++){
             
                 suma=suma+arreglo[i];
                             }
         media=suma/tamaño;
-         System.out.println(suma);
-        System.out.println(media);
-        float rango=0;
+        
+        double rango;
         
         for(int i=0;i<tamaño;i++){
             
-            rango=(float) Math.pow((arreglo[i]-media),2);
+            rango= Math.pow((arreglo[i]-media),2);
             varianza= (varianza+rango);
         }
             varianza=varianza/(tamaño-1);           
-            System.out.println(varianza);
+            System.out.println("La varianza es: "+varianza);
         }
       
      public void Desviacion_e (){
+         
+         double suma=0;
               for(int i=0;i<tamaño;i++){
             
                 suma=suma+arreglo[i];
                             }
         media=suma/tamaño;
          
-        float rango=0;
+        double rango;
         
         for(int i=0;i<tamaño;i++){
             
-            rango=(float) Math.pow((arreglo[i]-media),2);
+            rango= Math.pow((arreglo[i]-media),2);
             varianza= (varianza+rango);
         }
             varianza=varianza/(tamaño-1);  
-            desviacion_e=(float) Math.sqrt(varianza);
-            System.out.println(desviacion_e);
+            desviacion_e= Math.sqrt(varianza);
+            System.out.println("La desciacion estandar es: "+desviacion_e);
         }
-      
-    }
+}
 
-        
+   
--- Codigos_informe_2/src/codigos_informe_2/Codigos_informe_2.java
+++ Codigos_informe_2/src/codigos_informe_2/Codigos_informe_2.java
@@ -10,30 +10,47 @@ public class Codigos_informe_2 {
 
     public static void main(String[] args) {
         Scanner entra= new Scanner(System.in);
+                
         String opcion2;
-        int opcion, tamaño=0, lugar=0, arreglo[]={0};
-        float media=0, suma=0;
-                        
+        int opcion, opcion3;
         Calculos c1= new Calculos();
-        c1.Llenar( lugar);
-        do{
-         System.out.println("Ingrese la opcion que desea realizar con el arreglo: \n1. Varianza \n2. Media \n3. Desviación estandar");
-         opcion=entra.nextInt();
-         switch (opcion){
-             case 1:
-                 c1.Varianza();
-                 break;
-             case 2:
-                 c1.Media();
-                 break;
-             case 3:
-                 c1.Desviacion_e();
+                
+        System.out.println("\t Programa puntos informe 2");
+        System.out.println("Ingrese los puntos que desea realizar: ");
+        System.out.println("1. Puntos 1 y 2 \n2. Puntos 3 y 4 \n3. Puntos 5 y 6");
+        opcion3=entra.nextInt();
+        switch (opcion3){
+            
+            case 1:             
+                    
+                    c1.Llenar_numeros();
+                do{
+                     System.out.println("Ingrese la opcion que desea realizar con el arreglo: \n1. Varianza \n2. Media \n3. Desviación estandar");
+                     opcion=entra.nextInt();
+                 switch (opcion){
+                        case 1:
+                            c1.Varianza();
+                         break;
+                        case 2:
+                            c1.Media();
+                        break;
+                        case 3:
+                            c1.Desviacion_e();
+                        break;
+                            }
+                    System.out.println("¿Desea volver a ejecutar el programa?");
+                    opcion2=entra.nextLine();
+                    opcion2=entra.nextLine();
+                    opcion2.toLowerCase();
+                    }while(opcion2.equals("si")==true);   
+                break;
+            
+            case 2:
+                c1.Llenar_letras();
+                c1.Repeticiones();
          }
-            System.out.println("¿Desea volver a ejecutar el programa?");
-            //opcion2=entra.nextLine();
-            opcion2=entra.nextLine();
-            opcion2.toLowerCase();
-        }while(opcion2.equals("si")==true);    
+    
+    
     }
     } 
     
